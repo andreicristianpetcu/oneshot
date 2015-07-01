c25bd9f792070e8107083a6ceccc37be2f6c8508
Andrei Cristian Petcu <andrei.petcu@cegeka.com>
2015-07-01 19:04:09 +0300

    added correct SIP server

--- Ubuntu-1404/sky_skype_for_business/install_sky.sh
+++ Ubuntu-1404/sky_skype_for_business/install_sky.sh
@@ -4,3 +4,7 @@ set -e
 wget http://tel.red/linux/sky_ubuntu64_v2.0.355.deb -O /tmp/sky_ubuntu64.deb
 sudo dpkg -i /tmp/sky_ubuntu64.deb
 rm -rf /tmp/sky_ubuntu64.deb
+
+mkdir -p $HOME/.config/tel.red
+cp Sky.conf $HOME/.config/tel.red/Sky.conf
+
